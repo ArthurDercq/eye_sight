d8aef1ba708bf3a8da9b672422bfbeb2583aa06e
Arthur Dercq <arthur.dercq@skynet.be>
2025-07-25 11:11:56 +0200

    update

--- eye_sight/app/app.py
+++ eye_sight/app/app.py
@@ -1,9 +1,21 @@
 from eye_sight.main import *
+import streamlit as st
+from sqlalchemy import create_engine
+from eye_sight.params import DB_URI
 
 
 #Get data
 
-activities = fetch_strava_data()
-activities_clean = clean_data(activities)
+@st.cache_data
+def load_data():
+    engine = create_engine(DB_URI)
+    query = "SELECT * FROM activities"
+    df = pd.read_sql(query, engine)
+    return df
+
+# Appel dans Streamlit
+st.title("Mes activités Strava")
+df = load_data()
+st.dataframe(df)
 
 #KPIs to display
